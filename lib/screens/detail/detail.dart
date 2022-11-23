d05be6fbacd92f7eddf7ee4a8107858e8f1a2b53
kamrunz <kamraan.namazov@gmail.com>
2022-11-23 18:42:39 +0400

    house detatil info

--- lib/screens/detail/detail.dart
+++ lib/screens/detail/detail.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:rent_app/models/house.dart';
+import 'package:rent_app/widgets/detail/content_intro.dart';
 import 'package:rent_app/widgets/detail/detail_app_bar.dart';
+import 'package:rent_app/widgets/detail/house_info.dart';
 
 class DetailPage extends StatelessWidget {
   final House house;
@@ -11,8 +13,15 @@ class DetailPage extends StatelessWidget {
     return Scaffold(
       body: SingleChildScrollView(
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             DetailAppBar(house: house),
+            const SizedBox(height: 20),
+            ContentIntro(house: house),
+            SizedBox(
+              height: 20,
+            ),
+            HouseInfo(),
           ],
         ),
       ),
