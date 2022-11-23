a530773644b2cf3ad4edc90fec0ec22a4e42a72c
kamrunz <kamraan.namazov@gmail.com>
2022-11-23 18:45:03 +0400

    last commit

--- lib/screens/detail/detail.dart
+++ lib/screens/detail/detail.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:rent_app/models/house.dart';
+import 'package:rent_app/widgets/detail/about.dart';
 import 'package:rent_app/widgets/detail/content_intro.dart';
 import 'package:rent_app/widgets/detail/detail_app_bar.dart';
 import 'package:rent_app/widgets/detail/house_info.dart';
@@ -11,20 +12,36 @@ class DetailPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            DetailAppBar(house: house),
-            const SizedBox(height: 20),
-            ContentIntro(house: house),
-            SizedBox(
-              height: 20,
-            ),
-            HouseInfo(),
-          ],
-        ),
-      ),
-    );
+        body: SingleChildScrollView(
+            child: Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        DetailAppBar(house: house),
+        SizedBox(height: 20),
+        ContentIntro(house: house),
+        SizedBox(height: 20),
+        HouseInfo(),
+        SizedBox(height: 20),
+        About(),
+        SizedBox(height: 25),
+        Container(
+            padding: EdgeInsets.symmetric(horizontal: 20),
+            child: ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8)),
+                  primary: Theme.of(context).primaryColor,
+                ),
+                child: Container(
+                    alignment: Alignment.center,
+                    padding: EdgeInsets.symmetric(vertical: 15),
+                    child: Text('Book Now',
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold)))))
+      ],
+    )));
   }
 }
